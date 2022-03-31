1b5913ad63ebb7e03f0452cb6f2ad7d04c9c7ff0
Olivier Blanc <olivier3lanc@gmail.com>
2022-03-31 22:44:24 +0200

    Upgrade LibDoc

--- libdoc/js/template.js
+++ libdoc/js/template.js
@@ -74,7 +74,8 @@ jQuery(document).ready(function() {
         if (jQuery(this).closest('.playground').length == 0) {
             var link = jQuery(this).attr('href');
             //Check if it is an internal link (check if hostname is contained into the link string)
-            if(link.indexOf(window.location.origin) != 0){
+            const location_host_pathname = location.host + location.pathname;
+            if(link.indexOf(location_host_pathname) == -1) {
                 jQuery(this)
                     .append('<span class="i-external-link u-ml-xxs"></span>')
                     // .append(' <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg> ')

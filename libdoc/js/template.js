3c9c15330ef0cca0c7980056fcdba894715d0b1c
Olivier Blanc <olivier3lanc@gmail.com>
2022-05-09 20:56:46 +0200

    Upgrade LibDoc

--- libdoc/js/template.js
+++ libdoc/js/template.js
@@ -69,18 +69,25 @@ let copyToClipboard = function(value) {
 jQuery(document).ready(function() {
     myToggle.update();
     // EXTERNAL LINKS MANAGEMENT
-    jQuery('main a[href^="http"]:not(#libdoc-codemirror-external-link), .libdoc-sidebar-item a[href^="http"]').each(function(){
+    jQuery('main a[href^="http"]:not(#libdoc-codemirror-external-link), .libdoc-sidebar-item a[href^="http"]:not([href*="/?iframe_mode="])').each(function(){
         // Only if link is not in .playground
         if (jQuery(this).closest('.playground, [data-playground-commands], #libdoc-assets').length == 0) {
-            var link = jQuery(this).attr('href');
+            var link = jQuery(this)[0].href;
             //Check if it is an internal link (check if hostname is contained into the link string)
-            const location_host_pathname = location.host + location.pathname;
+            const location_host_pathname = location.origin;
             if(link.indexOf(location_host_pathname) == -1) {
                 jQuery(this)
                     .append('<span class="i-external-link u-ml-xxs"></span>')
                     // .append(' <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg> ')
                     .attr('target','_blank');
             }
+        }
+    });
+
+    // DOWNLOAD LINKS MANAGEMENT
+    document.querySelectorAll('main a[download]').forEach(function(el) {
+        if (el.querySelector('.i-download') === null) {
+            el.insertAdjacentHTML('beforeend', '<span class="i-download u-ml-xxs"></span>');
         }
     });
 
@@ -301,58 +308,54 @@ let resizer = {
 }
 resizer.update();
 
-// PAGE FEATURED PLAYGROUND
-// Get content from the URL and apply the playground
-// iframe next to the code syntax highlighter  
-const pageFeaturedPlayground = {
-    update: function() {
-        const   el_iframe_container = document.getElementById('libdoc-page-split'),
-                el_featured_code = document.getElementById('libdoc-featured-code'),
-                hash = window.location.hash.replace('#', '');
-
-        if (el_iframe_container !== null && el_featured_code !== null && hash.length > 0) {
-            const   stringified_sent_object = atob(hash),
-                    sent_object = JSON.parse(stringified_sent_object),
-                    sent_object_escaped = sent_object.content.replace(/&/g, "&amp;")
-                                                .replace(/</g, "&lt;")
-                                                .replace(/>/g, "&gt;")
-                                                .replace(/"/g, "&quot;")
-                                                .replace(/'/g, "&#039;");
-            // console.log(`<pre><code class="language-html">${sent_object.content}</code></pre>`);
-            el_featured_code.innerHTML = `<pre class="playground" title="${sent_object.options.title}"><code class="language-html">${sent_object_escaped}</code></pre>`;
-            // Prism.highlightAll();
-            // el_iframe_container.innerHTML = '<iframe src="'+site.url+site.baseurl+'/libdoc/playground.html#'+hash+'" class="u-transition-none u-absolute u-h-100 u-b-none u-w-100"></iframe>';
-        }
-    }
-}
-pageFeaturedPlayground.update();
-
 // IFRAME MODE
 const iframeModeEmbed = function(url) {
     if (typeof url == 'string') {
         const el_main = document.querySelector('main');
         if (el_main !== null) {
-            el_main.innerHTML = '<iframe src="'+url+'" class="u-h-100vh u-w-100 u-bl-none u-bt-none u-br-none u-bb-thin-dashed-alt"></iframe>';
+            const el_iframe_embed = document.createElement('iframe');
+            const matching_url = site.url+site.baseurl+'/?iframe_mode='+url;
+            el_iframe_embed.onload = function() {
+                // Update page title
+                try {
+                    // Iframe in on the same host
+                    document.title = el_iframe_embed.contentWindow.document.title;
+                } catch (e) {
+                    // CORS mismatch, just use the link text as document title
+                    const el_source_link = document.querySelector('.libdoc-sidebar-item > a[href="'+matching_url+'"]');
+                    if (el_source_link !== null) {
+                        document.title = el_source_link.innerText;
+                    }
+                }
+            }
+            el_iframe_embed.src = url;
+            el_iframe_embed.classList.add('u-h-100vh', 'u-w-100', 'u-bl-none', 'u-bt-none', 'u-br-none', 'u-bb-thin-dashed-alt');
+            el_main.innerHTML = '';
+            el_main.appendChild(el_iframe_embed);
             sidebar('close');
-            history.pushState(null, null, '?iframe_mode='+url);
+            // history.pushState(null, null, '?iframe_mode='+url);
             // Update link states into libdoc's navbar
-            document.querySelectorAll('.libdoc-sidebar-item a').forEach(function(el) {
-                const href = el.href;
-                if (href == url) {
+            document.querySelectorAll('.libdoc-sidebar-item').forEach(function(el_item) {
+                const el = el_item.querySelector('a');
+                if (el.href == matching_url) {
+                    el_item.classList.add('libdoc-sidebar-current-item');
                     el.classList.add('u-br-large-solid', 'u-bc-primary-edge');
+                    el.classList.remove('m-ff-lead');
                 } else {
+                    el_item.classList.remove('libdoc-sidebar-current-item');
                     el.classList.remove('u-br-large-solid', 'u-bc-primary-edge');
+                    el.classList.add('m-ff-lead');
                 }
             });
         }
     }
 }
-document.querySelectorAll('#libdoc-sidebar a[data-iframe-mode="true"]').forEach(function(el) {
-    el.addEventListener('click', function(e) {
-        e.preventDefault();
-        iframeModeEmbed(el.href);
-    })
-});
+// document.querySelectorAll('#libdoc-sidebar a[data-iframe-mode="true"]').forEach(function(el) {
+//     el.addEventListener('click', function(e) {
+//         e.preventDefault();
+//         iframeModeEmbed(el.href);
+//     })
+// });
 
 // LOAD IFRAME AT PAGE LOAD
 // If URL has iframe_mode as GET param
@@ -368,12 +371,9 @@ if (el_libdoc_sidebar_menu !== null) {
     const el_libdoc_sidebar_current_item = el_libdoc_sidebar_menu.querySelector('.libdoc-sidebar-current-item');
     if (el_libdoc_sidebar_current_item !== null) {
         const libdoc_sidebar_current_item_offset_top = el_libdoc_sidebar_current_item.offsetTop;
-        const libdoc_sidebar_current_item_offset_threshold = window.innerHeight - 2 * el_libdoc_sidebar_current_item.clientHeight;
-        if (libdoc_sidebar_current_item_offset_top > libdoc_sidebar_current_item_offset_threshold) {
-            el_libdoc_sidebar_menu.scrollTo({
-                top: libdoc_sidebar_current_item_offset_top,
-                behavior: 'smooth'
-            });
-        }
+        el_libdoc_sidebar_menu.scrollTo({
+            top: libdoc_sidebar_current_item_offset_top,
+            behavior: 'smooth'
+        });
     }
 }
